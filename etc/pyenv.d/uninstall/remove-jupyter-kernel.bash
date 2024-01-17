dd7700ddd4319c30cf796f7a45ca509da57f6fa0
Jongsu Liam Kim <jongsukim8@gmail.com>
2024-01-17 20:35:44 +0900

    Change register-kernel to manual registration
    * Why: Automatic registration has an incorrect assumption that jupyter must be installed globally
    * Remove batch script for adding jupyter kernels
    * Remove after_install hook because it is redundant now

--- etc/pyenv.d/uninstall/remove-jupyter-kernel.bash
+++ etc/pyenv.d/uninstall/remove-jupyter-kernel.bash
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
+env_name=$1
 
-if declare -Ff after_uninstall >/dev/null; then
-    after_uninstall 'rm -rf $(jupyter --data-dir)/kernels/${VERSION_NAME}'
+check_python_package_installed() {
+    local package=$1
+    PYENV_VERSION=$env_name pyenv exec pip list --disable-pip-version-check | grep -F "$package" > /dev/null
+    if [ $? -eq 0 ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+if declare -Ff before_uninstall >/dev/null; then
+    if check_python_package_installed "jupyter"; then
+        jupyter_data_dir=$(PYENV_VERSION=$env_name pyenv exec jupyter --data-dir)
+        before_uninstall 'rm -rf ${jupyter_data_dir}/kernels/pyenv_${env_name}'
+    fi
 else
     echo "pyenv: pyenv-jupyter-kernel plugin requires pyenv v0.1.0 or later" >&2
 fi
